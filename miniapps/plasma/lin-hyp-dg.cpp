83cbdffdfd8caa1f27c6770869c0aaad5194e017
rhealbarnett <rhealbarnett@gmail.com>
2022-09-21 11:41:25 -0400

    Parse options & include mesh file.

--- miniapps/plasma/lin-hyp-dg.cpp
+++ miniapps/plasma/lin-hyp-dg.cpp
@@ -29,25 +29,60 @@ const double vs = 1.0;
 // define number of equations ? (taken from ex 14)
 const int num_equations = 2;
 
-//////// START MAIN /////////
-int main()
-{
-
-    // Initialise MPI and HYPRE
-    Mpi::Init
-/*
-define mesh file: can be simple uniform grid to start e.g. slab_128.mesh.
-*/
-    
-
-
-/*
-do we need mesh refinement? might be good to include for future...?
-*/
-
 /*
 need to write class for DG Solver
 */
+
+//////// START MAIN /////////
+int main(int argc, char *argv[])
+{
+
+    // Initialise MPI and HYPRE
+    Mpi::Init(argc, argv);
+    int num_procs = Mpi::WorldSize();
+    int myid = Mpi::WorldRank();
+    Hypre::Init();
+    bool verbose = (myid == 0);
+
+    // parse command line options
+    const char *mesh_file = "~/Documents/git-repos/RF-SciDAC/mfem-analysis/1D_tests/linear-hyp/slab_128.mesh";
+    int precision 16;
+    cout.precision(precision);
+
+    OptionsParser args(argc,argv);
+    args.AddOption(&mesh_file, "-m", "--mesh",
+                    "Mesh file to use");
+    args.Parse();
+    if (!args.Good())
+    {
+        if (Mpi::Root)
+        {
+            args.PrintUsage(cout);
+        }
+        return 1;
+    }
+    if (Mpi::Root())
+    {
+        args.PrintOptions(cout);
+    } 
+
+/*
+define mesh file: can be simple uniform grid to start e.g. slab_128.mesh.
+*/
+
+    // Read mesh file
+    // Not including refinement for now but 
+    Mesh mesh(mesh_file, 1, 1);
+    const int dim = mesh.Dimension(); 
+    /*
+    What's the difference between defining the mesh dimension as 
+        const int dim =  mesh.Dimension()
+    and
+        int dim = mesh->Dimension();
+    ??
+    */
+    
+
 
 /*
 write class for FE evolution
